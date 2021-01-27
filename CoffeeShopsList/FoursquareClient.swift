709ed4ade14f0faf9c08a44253ea97bffb8f4cfc
VenkataMadira <madira1587@gmail.com>
2021-01-27 13:19:58 +0000

    first commit

---
+++ CoffeeShopsList/FoursquareClient.swift
@@ -0,0 +1,8 @@
+//
+//  FoursquareClient.swift
+//  CoffeeShopsList
+//
+//  Created by Venkat Madira on 25/01/2021.
+//
+
+import Foundation
---
+++ CoffeeShopsList/FoursquareModel.swift
@@ -0,0 +1,8 @@
+//
+//  FoursquareModel.swift
+//  CoffeeShopsList
+//
+//  Created by Venkat Madira on 25/01/2021.
+//
+
+import Foundation
